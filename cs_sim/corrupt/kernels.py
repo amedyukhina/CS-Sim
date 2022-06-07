863f1ce21802f1b9aa223545b26cf70012983062
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-06-07 16:51:44 -0500

    gaussian kernel

---
+++ cs_sim/corrupt/kernels.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy import ndimage
+
+
+def gaussian_kernel(sigma, scale: int = 4):
+    sigma = np.array([sigma]).flatten()
+
+    # calculate the size of the output array based on sigma and scale; make sure it is non-zero
+    size = np.int_(np.round_(sigma))
+    size[np.where(size < 1)] = 1
+    size = size * 2 * scale + 1
+
+    kernel = np.zeros(size)  # create an empty array
+    kernel[tuple(np.int_(size / 2))] = 255.  # create a peak in the center of the kernel
+    kernel = ndimage.gaussian_filter(kernel, sigma)  # smooth the peak with a Gaussian
+    kernel = kernel / np.sum(kernel)  # normalize the kernel
+    return kernel
