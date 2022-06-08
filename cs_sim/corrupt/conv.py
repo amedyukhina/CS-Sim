e74798abfcd03c44941a67b5dadf2f6975eb109e
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-06-08 12:36:28 -0500

    option to convolve with gauss of given sigma

--- cs_sim/corrupt/conv.py
+++ cs_sim/corrupt/conv.py
@@ -1,6 +1,18 @@
+import numpy as np
 from scipy.signal import fftconvolve
 
+from .kernels import gaussian_kernel
 
-def convolve(img, psf, mode='same'):
+
+def convolve(img, psf=None, sigma=None, ndim=3, mode='same'):
+    if psf is None:
+        if sigma is not None:
+            sigma = np.ravel([sigma])
+            if len(sigma) < ndim:
+                sigma = np.array([sigma[0]] * ndim)
+            psf = gaussian_kernel(sigma)
+        else:
+            raise ValueError("Either psf or sigma must be provided")
     imgf = fftconvolve(img * 1., psf * 1., mode=mode)
+
     return imgf
--- cs_sim/tests/test_conv.py
+++ cs_sim/tests/test_conv.py
@@ -1,7 +1,24 @@
 import numpy as np
+import pytest
+
 from ..corrupt.conv import convolve
 
 
+@pytest.fixture(params=[1, 3, 10])
+def sigmas(request):
+    return request.param
+
+
 def test_gauss(line_imgs, gauss_kernels):
-    img = convolve(line_imgs, gauss_kernels)
+    img = convolve(line_imgs, psf=gauss_kernels)
     assert np.max(img) > 0
+
+
+def test_gauss_with_sigma(line_imgs, sigmas):
+    img = convolve(line_imgs, sigma=sigmas)
+    assert np.max(img) > 0
+
+
+def test_conv_error(line_imgs):
+    with pytest.raises(ValueError):
+        img = convolve(line_imgs)
