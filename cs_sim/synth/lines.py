3e399a352393d79f115c1c0a901c24f447053d9c
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-06-08 12:19:35 -0500

    bug fix

--- cs_sim/synth/lines.py
+++ cs_sim/synth/lines.py
@@ -8,5 +8,5 @@ def generate_img_with_lines(imgshape, n_lines=10, maxval=255, nval=100):
         for j in range(len(imgshape)):
             inds = np.random.randint(0, imgshape[j], 2)
             coords.append(np.linspace(inds[0], inds[1], nval, endpoint=True))
-        img[np.int_(np.round_(coords))] = maxval
+        img[tuple(np.int_(np.round_(coords)))] = maxval
     return img
--- cs_sim/tests/test_lines.py
+++ cs_sim/tests/test_lines.py
@@ -5,4 +5,5 @@ def test_lines(line_params):
     size = line_params.pop('size')
     img = generate_img_with_lines(imgshape=[size] * 3, **line_params)
     assert img.max() == line_params['maxval']
+    assert img.min() == 0
     assert img.shape == (size, size, size)
